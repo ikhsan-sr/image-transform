a16859dc688279ac8ec071be6908bf091f4122bd
Muhammad Ikhsan <ikhsan.ssr@gmail.com>
2024-07-25 15:52:06 +0700

    chore: upgdate aws sdk v2 to v3

--- helpers/s3Helper.js
+++ helpers/s3Helper.js
@@ -1,25 +1,28 @@
-const AWS = require('aws-sdk');
+const { S3Client } = require('@aws-sdk/client-s3');
+const { Upload } = require('@aws-sdk/lib-storage');
+
 const mime = require('mime-types');
 const { AWS_HOSTNAME, AWS_BUCKET_NAME, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY } = process.env;
 
-AWS.config.update({
+const s3Client = new S3Client({
   endpoint: AWS_HOSTNAME,
   accessKeyId: AWS_ACCESS_KEY_ID,
   secretAccessKey: AWS_SECRET_ACCESS_KEY,
 });
 
-const s3 = new AWS.S3();
-
 async function uploadToS3(buffer, key) {
   try {
-    const data = await s3.upload({
-      Bucket: AWS_BUCKET_NAME,
-      Key: key,
-      Body: buffer,
-      ContentType: mime.lookup(key) || 'image/jpeg',
-      ACL: 'public-read'
-    }).promise();
-    return data.Location;
+    const upload = new Upload({
+      client: s3Client, 
+      params: {
+        Bucket: AWS_BUCKET_NAME,
+        Key: key,
+        Body: buffer,
+        ContentType: mime.lookup(key) || 'image/jpeg',
+        ACL: 'public-read'
+      }
+    });
+    return upload.done();
   } catch (error) {
     console.error('Error uploading image to S3:', error);
     throw new Error('Failed to upload image');
