fb943137bbcefcfce9a9f544b1a6e8d48084a000
Muhammad Ikhsan <ikhsan.ssr@gmail.com>
2024-07-26 15:01:16 +0700

    feat: add image validation

--- routes/imageCompress.js
+++ routes/imageCompress.js
@@ -1,62 +1,70 @@
 const express = require('express');
 const path = require('path');
 const sizeOf = require('buffer-image-size');
-
 const { uploadToS3 } = require('../helpers/s3Helper');
 const { compressImage } = require('../helpers/imageHelper');
 const { downloadImage } = require('../utils/downloadUtils');
 const { createResponse } = require('../utils/responseUtils');
 const { validateImageUrl } = require('../utils/validationUtils');
-
 const { IMAGE_DIRECTORY, MAX_SIZE } = require('../constants/imageConstants');
 const SIZES = require('../constants/sizeConstants');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../index.html'));
-});
+router.get('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 router.get('/compress', async (req, res, next) => {
   const { url } = req.query;
 
-  // Validasi URL gambar
+  // Validate image URL
   const { isValid, message } = await validateImageUrl(url);
-
   if (!isValid) return res.status(400).json(createResponse(400, message));
 
   try {
+    // Download and get image dimensions
     const originalBuffer = await downloadImage(url);
     const originalWidth = sizeOf(originalBuffer)?.width;
-    const filename = path.basename(url, path.extname(url));
-    const originalExt = path.extname(url).split('.').pop();
-    const encodedUri = encodeURIComponent(filename);
+    const { base: filename, ext: originalExt } = path.parse(url);
 
-    const originalKey = `${IMAGE_DIRECTORY}/${encodedUri}.${originalExt}`;
+    const encodedFilename = encodeURIComponent(filename);
+
+    // Define keys for S3 storage
+    const originalKey = `${IMAGE_DIRECTORY}/${encodedFilename}${originalExt}`;
+    const maxKey = `${IMAGE_DIRECTORY}/${encodedFilename}_max${originalExt}`;
+
+    // Upload original image
     await uploadToS3(originalBuffer, originalKey);
 
+    // Compress and upload the image with maximum size
+    const maxBuffer = await compressImage(originalBuffer, MAX_SIZE, 80);
+    await uploadToS3(originalWidth > MAX_SIZE ? maxBuffer : originalBuffer, maxKey);
+
+    // Prepare output URLs
     const outputUrls = {
-      max: `${process.env.AWS_HOSTNAME}/${process.env.AWS_BUCKET_NAME}/${IMAGE_DIRECTORY}/${encodedUri}_max.${originalExt}`
+      max: `${process.env.AWS_HOSTNAME}/${process.env.AWS_BUCKET_NAME}/${maxKey}`,
     };
 
-    const maxBuffer = await compressImage(originalBuffer, MAX_SIZE, 80);
-    const maxKey = `${IMAGE_DIRECTORY}/${encodedUri}_max.${originalExt}`;
-    await uploadToS3( originalWidth > MAX_SIZE ? maxBuffer : originalBuffer, maxKey);
-
+    // Process and upload images for different sizes
     await Promise.all(SIZES.map(async ({ size, width, quality }) => {
-      const outputBufferWebP = await compressImage(originalBuffer, width, quality, 'webp');
-      const outputKeyWebP = `${IMAGE_DIRECTORY}/${encodedUri}_${size}.webp`;
-      await uploadToS3(outputBufferWebP, outputKeyWebP);
+      const sizeKeyBase = `${IMAGE_DIRECTORY}/${encodedFilename}_${size}`;
+      const webpKey = `${sizeKeyBase}.webp`;
+      const originalSizeKey = `${sizeKeyBase}${originalExt}`;
 
-      const outputBufferOriginal = await compressImage(originalBuffer, width, quality);
-      const outputKeyOriginal = `${IMAGE_DIRECTORY}/${encodedUri}_${size}.${originalExt}`;
-      await uploadToS3(outputBufferOriginal, outputKeyOriginal);
+      // Compress and upload WebP version
+      const webpBuffer = await compressImage(originalBuffer, width, quality, 'webp');
+      await uploadToS3(webpBuffer, webpKey);
 
-      outputUrls[size] = `${process.env.AWS_HOSTNAME}/${process.env.AWS_BUCKET_NAME}/${IMAGE_DIRECTORY}/${encodedUri}_${size}.${originalExt}`;
-      outputUrls[`${size}_webp`] = `${process.env.AWS_HOSTNAME}/${process.env.AWS_BUCKET_NAME}/${IMAGE_DIRECTORY}/${encodedUri}_${size}.webp`;
+      // Compress and upload original format version
+      const originalBufferSized = await compressImage(originalBuffer, width, quality);
+      await uploadToS3(originalBufferSized, originalSizeKey);
+
+      // Update output URLs
+      outputUrls[size] = `${process.env.AWS_HOSTNAME}/${process.env.AWS_BUCKET_NAME}/${originalSizeKey}`;
+      outputUrls[`${size}_webp`] = `${process.env.AWS_HOSTNAME}/${process.env.AWS_BUCKET_NAME}/${webpKey}`;
     }));
 
-    res.json(createResponse(200, 'Success compress image!', outputUrls));
+    // Send response with output URLs
+    res.json(createResponse(200, 'Image compression successful!', outputUrls));
   } catch (error) {
     next(error);
   }
